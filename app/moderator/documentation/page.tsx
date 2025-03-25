212511c66d7deed6db7beecf75f76bd7f5a00c86
kobe <kobecode24@outlook.com>
2025-03-25 15:33:16 +0000

    Replace AdminGuard with ModeratorGuard in ModeratorDocumentationPage to enforce access control for moderator users.

--- app/moderator/documentation/page.tsx
+++ app/moderator/documentation/page.tsx
@@ -3,17 +3,18 @@
 import { ForumHeader } from "@/components/forum-header"
 import { DocumentationPortal } from "@/components/moderator/documentation-portal"
 import { AdminGuard } from "@/components/guards/AdminGuard"
+import {ModeratorGuard} from "@/components/guards/ModeratorGuard";
 
 export default function ModeratorDocumentationPage() {
   return (
-    <AdminGuard>
+    <ModeratorGuard>
       <div className="min-h-screen flex flex-col bg-gradient-to-br from-[#0A192F] to-[#663399] text-white">
         <ForumHeader />
         <div className="flex-grow">
           <DocumentationPortal />
         </div>
       </div>
-    </AdminGuard>
+    </ModeratorGuard>
   )
 }
 
